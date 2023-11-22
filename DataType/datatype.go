fdcb31aeec652f223bb3f0e948237aedcf7166ea
abhinandpn <abhinandpn4067@gmail.com>
2023-11-22 23:21:44 +0530

    2 variable declaring and printing methond

--- DataType/datatype.go
+++ DataType/datatype.go
@@ -8,8 +8,16 @@ func Display() {
 	name := "abhinandpn" // short variable declartion (datatype >> string)
 	number := 1          // short variable declartion (datatype >> number)
 	float := 2.4         // short variable declartion (datatype >> float)4
-	
-	fmt.Println(name)    // printing command
+
+	fmt.Println(name) // printing command
 	fmt.Println(number)
 	fmt.Println(float)
+}
+
+func VarDisplay() {
+
+	first := "abhinand"
+	last := "pn"
+
+	fmt.Println("name is ", first+last)
 }
