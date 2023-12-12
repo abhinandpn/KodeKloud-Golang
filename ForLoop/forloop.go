c821f618c7ef12b5f5068f58886d672e5cb88b18
abhinandpn <abhinandpn4067@gmail.com>
2023-12-12 14:13:12 +0530

    for loop concepts

---
+++ ForLoop/forloop.go
@@ -0,0 +1,10 @@
+package forloop
+
+func Run() {
+	sum := 0
+	for {
+		sum++
+	}
+	// fmt.Println(sum)
+	// its infinite 
+}
--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	elseif "github.com/abhinandpn/KodeKloud-Golang/ElseIF"
-)
-
 func main() {
-	elseif.Run()
+
 }
